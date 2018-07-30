7e8dcc284d191acadb179ed1d0f4c0ceddec1f5e
delaya73 <delaya73@gmail.com>
2018-07-30 11:25:24 +0500

    Do not trigger power HAL with wakeup gesture
    Wakeup gesture is disabled so do not trigger power HAL
    by commenting out TARGET_TAP_TO_WAKE_NODE flag.
    
    link for info: mer-hybris/device-sony-suzu@e4a15da#diff-78ec901c9c29ef6a20b79592fd93f941

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -203,7 +203,7 @@ TARGET_USES_MKE2FS := true
 
 # Power
 TARGET_HAS_NO_WIFI_STATS := true
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
+#TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 TARGET_USES_INTERACTION_BOOST := true
 
 # RIL
